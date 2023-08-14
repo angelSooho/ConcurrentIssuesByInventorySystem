e3964e20a06f1a3082de4a47abe9499068765e56
SoohoLee <hyeri0603@naver.com>
2023-08-14 17:09:28 +0900

    feature: Redis Lettuce 구현

---
+++ src/main/java/com/example/concurrentissuesbyinventorysystem/facade/LettuceLockStockFacade.java
@@ -0,0 +1,29 @@
+package com.example.concurrentissuesbyinventorysystem.facade;
+
+import com.example.concurrentissuesbyinventorysystem.repository.RedisLockRepository;
+import com.example.concurrentissuesbyinventorysystem.service.StockService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LettuceLockStockFacade {
+
+    private final RedisLockRepository redisLockRepository;
+
+    private final StockService stockService;
+
+    public LettuceLockStockFacade(RedisLockRepository redisLockRepository, StockService stockService) {
+        this.redisLockRepository = redisLockRepository;
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        while (!redisLockRepository.lock(id)) {
+            Thread.sleep(100);
+        }
+        try {
+            stockService.decrease(id, quantity);
+        } finally {
+            redisLockRepository.unlock(id);
+        }
+    }
+}
