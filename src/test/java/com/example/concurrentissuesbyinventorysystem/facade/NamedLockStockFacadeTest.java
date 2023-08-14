c33e6af2b18bd05cfa0a87a301bbe5a8fa3280f7
SoohoLee <hyeri0603@naver.com>
2023-08-14 17:09:45 +0900

    test: 각 동시성 이슈 테스트 코드 구현

---
+++ src/test/java/com/example/concurrentissuesbyinventorysystem/facade/NamedLockStockFacadeTest.java
@@ -0,0 +1,59 @@
+package com.example.concurrentissuesbyinventorysystem.facade;
+
+import com.example.concurrentissuesbyinventorysystem.IntegrationTestSupport;
+import com.example.concurrentissuesbyinventorysystem.domain.Stock;
+import com.example.concurrentissuesbyinventorysystem.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NamedLockStockFacadeTest extends IntegrationTestSupport {
+
+    @Autowired
+    private NamedLockStockFacade namedLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @AfterEach
+    public void tearDown() {
+        stockRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("상품을 동시에 100명이 주문하면, NamedLock에 의해 동시성 문제가 발생하지 않고 재고가 0이 된다.")
+    @Test
+    public void sameTimeOrderTest() throws InterruptedException {
+        //given
+        Stock stock = new Stock(1L, 100L);
+        Stock savedStock = stockRepository.save(stock);
+
+        //when
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    namedLockStockFacade.decrease(savedStock.getId(), 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        //then
+        Stock findStock = stockRepository.findById(savedStock.getId()).orElseThrow();
+        assertThat(findStock.getQuantity()).isEqualTo(0L);
+    }
+}
---
+++ src/test/java/com/example/concurrentissuesbyinventorysystem/facade/OptimisticLockStockFacadeTest.java
@@ -0,0 +1,61 @@
+package com.example.concurrentissuesbyinventorysystem.facade;
+
+import com.example.concurrentissuesbyinventorysystem.IntegrationTestSupport;
+import com.example.concurrentissuesbyinventorysystem.domain.Stock;
+import com.example.concurrentissuesbyinventorysystem.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OptimisticLockStockFacadeTest extends IntegrationTestSupport {
+
+    @Autowired
+    private OptimisticLockStockFacade optimisticLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @AfterEach
+    public void tearDown() {
+        stockRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("상품을 동시에 100명이 주문하면, 낙관락에 의해 동시성 문제가 발생하지 않고 재고가 0이 된다.")
+    @Test
+    public void sameTimeOrderTest() throws InterruptedException {
+        //given
+        Stock stock = new Stock(1L, 100L);
+        Stock savedStock = stockRepository.save(stock);
+
+        //when
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    optimisticLockStockFacade.decrease(savedStock.getId(), 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        //then
+        Stock findStock = stockRepository.findById(savedStock.getId()).orElseThrow();
+        assertThat(findStock.getQuantity()).isEqualTo(0L);
+    }
+}
