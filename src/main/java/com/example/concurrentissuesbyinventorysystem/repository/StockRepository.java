8131f490e44a765f58135f51deb8b98ca3b71bcf
SoohoLee <hyeri0603@naver.com>
2023-08-14 17:08:35 +0900

    feature: 종류별 DB락 학습

---
+++ src/main/java/com/example/concurrentissuesbyinventorysystem/repository/StockRepository.java
@@ -0,0 +1,20 @@
+package com.example.concurrentissuesbyinventorysystem.repository;
+
+import com.example.concurrentissuesbyinventorysystem.domain.Stock;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id=:id")
+    Stock findByIdWithPessimisticLock(Long id);
+
+    @Lock(value = LockModeType.OPTIMISTIC)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithOptimisticLock(Long id);
+
+    Stock findByProductId(Long productId);
+}
---
+++ src/main/java/com/example/concurrentissuesbyinventorysystem/service/PessimisticLockStockService.java
@@ -0,0 +1,23 @@
+package com.example.concurrentissuesbyinventorysystem.service;
+
+import com.example.concurrentissuesbyinventorysystem.domain.Stock;
+import com.example.concurrentissuesbyinventorysystem.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PessimisticLockStockService {
+
+    private final StockRepository stockRepository;
+
+    public PessimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id);
+        stock.decrease(quantity);
+    }
+
+}
