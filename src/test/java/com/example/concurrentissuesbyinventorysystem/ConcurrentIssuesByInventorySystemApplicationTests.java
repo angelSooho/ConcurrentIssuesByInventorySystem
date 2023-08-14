85c526e2db5cc20fc8c978f3ac99f7244b1a5be2
SoohoLee <hyeri0603@naver.com>
2023-08-14 14:49:19 +0900

    project init

---
+++ src/test/java/com/example/concurrentissuesbyinventorysystem/ConcurrentIssuesByInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concurrentissuesbyinventorysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentIssuesByInventorySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
